8f272eb13e8bb3f367b323f537951be19f73920c
shigemk2 <i.am.shige@gmail.com>
2016-09-25 18:37:48 +0900

    Add main

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,6 +1,16 @@
 package com.example
 
+import akka.actor.{ActorSystem, Props}
+
 object Hello {
   def main(args: Array[String]): Unit = {
+    val system = ActorSystem("mySystem")
+    val props = Props[MyActor]
+    val actor = system.actorOf(props, name = "myActor")
+
+    while (true) {
+      actor ! "hi"
+      Thread.sleep(1000)
+    }
   }
 }
